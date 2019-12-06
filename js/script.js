6137f9fdcfaab433856194ec0711baf6011f6abb
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-12-05 23:03:15 -0600

    first commit. Hero section setup

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// let h1 = document.getElementsByTagName('h1')[0]
+
+// h1.addEventListener('click', () => {
+//   h1.innerHTML = 'Treehouses'
+
+//   if (h1.innerHTML == 'Treehouses') {
+//     h1.style.cssText = ' color : red; opacity : 50%; font-size : 3rem;'
+
+//     console.log(`welcome to ${h1.innerHTML} Website`)
+//   }
+// })
+
+// if (h1.innerHTML == 'Test') {
+//   h1.style.cssText = ' color : #fff; opacity : 100%; font-size : 5rem;'
+
+//   console.log('Alert! Alert! this is just a test')
+// }
+
+const person = {
+  firstName: 'Nathan',
+  LastName: 'Daniels'
+}
+
+const { firstName: fname, lastName: lname } = person
